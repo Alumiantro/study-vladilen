a748cc2898c14541bd4dbff1bd6851bafdc449d5
adelya <zaikamarta@mail.ru>
2022-02-08 21:30:01 +0300

    studied the topic asynchronys

--- code.js
+++ code.js
@@ -8,7 +8,7 @@
 // clearTimeout(timeout) // сработает
 
 // const interval = setInterval(() => {
-         // clearInterval(interval) // 1 интервал сработает, остальные - нет 
+// clearInterval(interval) // 1 интервал сработает, остальные - нет 
 //         console.log('After Interval')
 //     }, 1000)
 // clearInterval(interval) // функция не будет отображаться вообще
@@ -22,7 +22,32 @@
 // }, 2000)
 
 const delay = (wait = 1000) => {
-    setTimeout(() => {
-
-    }, wait)
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve
+            // reject('Что-то пошло не так')
+        }, wait)
+    })
+    return promise
 }
+
+// delay(2500)
+//     .then(() => {
+//         console.log('After 2 second')
+//     })
+//     .catch(err => console.error('Error:', err))
+//     .finally(() => console.log('Finally'))
+
+
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5
+]))
+
+// getData().then(data => console.log(data)) 
+
+async function asyncExample() { // async делает функцию ассинхронной
+    await delay(2500) // хотим дождаться выполнения этой функции, и потом переходить в getdata
+    const data = await getData()
+    console.log('Data', data)
+}
+asyncExample()
